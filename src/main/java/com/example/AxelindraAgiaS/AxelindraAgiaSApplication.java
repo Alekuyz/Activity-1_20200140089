49d5f58b38f7c44563badac950e1213364c1b20e
Alekuyz <axelagia@gmail.com>
2021-12-20 13:45:16 +0700

    Alek

---
+++ src/main/java/com/example/AxelindraAgiaS/AxelindraAgiaSApplication.java
@@ -0,0 +1,13 @@
+package com.example.AxelindraAgiaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxelindraAgiaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxelindraAgiaSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AxelindraAgiaS/AxelindraAgiaSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AxelindraAgiaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxelindraAgiaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
